415f13fe3be953ea1cde46ba4bce73e68f8293a7
mono <mono@monoweb.info>
2011-04-02 21:52:49 +0900

    Initial commit

---
+++ TwitterTimelineFilter.pack.cmd
@@ -0,0 +1,1 @@
+"%HOME%\AppData\Local\Google\Chrome\Application\chrome.exe" "--pack-extension=%CD%\TwitterTimelineFilter" "--pack-extension-key=%CD%\TwitterTimelineFilter.pem" --no-message-box
---
+++ TwitterTimelineFilter/content.js
@@ -0,0 +1,63 @@
+(function() {
+
+if(!document.getElementById('doc')) {
+  return;
+}
+
+var filters = [];
+
+var port = chrome.extension.connect({ name: 'TwitterTimelineFilter' });
+port.onMessage.addListener(function(m) {
+  switch(m.type) {
+    case 'options.load':
+      filters = m.options.filters;
+      break;
+  }
+});
+port.postMessage({ type: 'options.load' });
+
+document.addEventListener('DOMNodeInserted', function(e) {
+  var element = e.target;
+  if(!element.className || element.className.indexOf('stream-item') == -1) {
+    return;
+  }
+  var tweetContent = element.querySelector('.stream-item-content .tweet-content');
+  var tweet = tweetContent.parentNode.parentNode;
+
+  var text = tweetContent.querySelector('.tweet-text').textContent;
+  var screenName = tweetContent.querySelector('.tweet-screen-name').textContent;
+  var fullName = tweetContent.querySelector('.tweet-full-name').textContent;
+
+  if(match(text, screenName, fullName)) {
+    element.style.display = 'none';
+  }
+}, false);
+
+var match = function(text, screenName, fullName) {
+  return filters.some(function(filter) {
+    var target = null;
+    switch(filter.type) {
+      case 'text':
+        target = text;
+        break;
+      case 'screenName':
+        target = screenName;
+        break;
+      case 'fullName':
+        target = fullName;
+        break;
+      default:
+        return false;
+    }
+    var pattern = filter.pattern;
+    if(filter.regexp) {
+      return RegExp(pattern).test(target);
+    }
+    else {
+      return target.indexOf(pattern) != -1;
+    }
+  });
+};
+
+})();
+
